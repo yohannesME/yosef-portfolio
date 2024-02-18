41ee7f603ed714a5b5d0076bb4c390266d46a7cc
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2024-02-18 21:24:55 +0300

    Update PortfolioSection.vue

--- src/components/portfolio/PortfolioSection.vue
+++ src/components/portfolio/PortfolioSection.vue
@@ -1,30 +1,42 @@
 <template>
-  <h2 class="heading" id="portfolio">My <span>Portfolio</span></h2>
-  <div class="portfolio">
-    <div class="box-container">
-      <div class="box" v-for="project in portfolio" :key="project.title">
-        <img :src="project.img" alt="" />
-        <div class="content">
-          <h3>{{ project.title }}</h3>
-          <span class="highlight">{{ project.description }}</span
-          ><br />
-          <span class="span">{{ project.techStack.toString() }}</span>
-          <a v-show="project.repo" :href="project.repo"
-            ><i class="fab fa-github"></i
-          ></a>
-          <a v-show="project.link" :href="project.link" target="_blank"
-            ><i class="fas fa-link"></i
-          ></a>
+  <div>
+    <h2 class="heading" id="portfolio">My <span>Portfolio</span></h2>
+    <div class="portfolio">
+      <div class="box-container">
+        <div class="box" v-for="project in portfolio" :key="project.title">
+          <img :src="project.img" alt="" />
+          <div class="content">
+            <h3>{{ project.title }}</h3>
+            <span class="highlight">{{ project.description }}</span><br />
+            <span class="span">{{ project.techStack.toString() }}</span>
+            <a v-show="project.repo" :href="project.repo">
+              <i class="fab fa-github"></i>
+            </a>
+            <a v-show="project.link" :href="project.link" target="_blank">
+              <i class="fas fa-link"></i>
+            </a>
+          </div>
+        </div>
+        <a
+          class="btn cv"
+          href="https://docs.google.com/presentation/d/1WnK-4pYV2jrD1bWB3T9-kic-BT2YzZkrYcaG1hkmkX0/edit?usp=sharing"
+          target="_blank"
+          type="button"
+        >
+          Slide Deck
+        </a>
+
+        <div class="video-container">
+          <iframe
+            width="560"
+            height="315"
+            :src="getYouTubeEmbedUrl('rKU8p3ft5ys')"
+            frameborder="0"
+            allowfullscreen
+          ></iframe>
         </div>
       </div>
     </div>
-    <a
-      class="btn cv"
-      href="https://docs.google.com/presentation/d/1WnK-4pYV2jrD1bWB3T9-kic-BT2YzZkrYcaG1hkmkX0/edit?usp=sharing"
-      target="_blank"
-      type="button"
-      >Slide Deck</a
-    >
   </div>
 </template>
 
@@ -36,6 +48,11 @@ export default {
     return {
       portfolio,
     };
+  },
+  methods: {
+    getYouTubeEmbedUrl(videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    },
   },
 };
 </script>
@@ -52,7 +69,7 @@ export default {
 }
 
 .portfolio .box-container .box {
-  height: 25rem;
+  height: 30rem;
   border-radius: 0.5rem;
   overflow: hidden;
   /* box-shadow: var(--box-shadow); */
@@ -109,4 +126,33 @@ export default {
   transform: translateY(0rem);
   opacity: 1;
 }
+
+.portfolio .box-container {
+  /* existing styles */
+
+  /* Add the following styles to create a grid layout */
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(27rem, 1fr));
+  gap: 2rem;
+
+}
+
+.video-container {
+  /* Add the following styles to make the video container responsive */
+  position: relative;
+  width: 100%;
+    
+
+}
+
+.video-container video {
+  /* Add the following styles to make the video fill the container */
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
 </style>
